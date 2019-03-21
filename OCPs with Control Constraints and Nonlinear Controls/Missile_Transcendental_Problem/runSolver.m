19ec8ad5935518f19edc2a6344b421049b561200
Kshitij Mall <mall@purdue.edu>
2019-03-21 00:15:07 -0400

    Removing files added in the wrong folder

--- OCPs with Control Constraints and Nonlinear Controls/Missile_Transcendental_Problem/runSolver.m
+++
@@ -1,9 +0,0 @@
-if ~exist('./autocode','dir')
-	mkdir('autocode');
-end
-if ~exist('./data','dir')
-	mkdir('data');
-end
-
-% runCombinedProcess(@new_thrust_aoaapprox);
-runCombinedProcess(@new_thrust_aoadot);
